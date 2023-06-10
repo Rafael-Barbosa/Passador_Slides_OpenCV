47989c3ea20e338e2947ef4c7e7edd571a3d26bd
Rafael Rodrigues Barbosa <rafael_master00@hotmail.com>
2023-06-10 06:54:42 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Passador Slides OpenCV
+  <h1 align="center">Passador Slides OpenCV </h1>
 
+
+
+### Este projeto utiliza a biblioteca OpenCV, juntamente com MediaPipe e Pynput, para criar um passador de slides controlado por gestos. Usamos a detecção de mãos do MediaPipe para identificar a posição dos dedos e os gestos, e o Pynput para controlar as ações do teclado.
+
+
+![Fig.gif](img/Ping-Pong.gif)
+
+# Dependências
+
+### - Python 3.10 🐍
+### - OpenCV
+### - MediaPipe
+### - Numpy
+### - Pynput
+
+
+# Instalação dos Pacotes 🔧
+
+```bash
+pip install -r requirements.txt
+```
+
+# Executando o jogo ▶️
+
+#### Certifique-se de ter todas as dependências instaladas. Em seguida, você pode executar o script em um terminal.
+
+#### Nota: Este programa usa a webcam para rastrear os gestos das mãos. Certifique-se de que você tenha uma webcam funcionando e acessível.
+
+
+```bash
+python main.py
+```
+
+##### Selecione uma webcam, o parâmetro `0` é a câmera default do seu sistema operacional.
+
+```python
+cap = cv2.VideoCapture(0) #Selecionar a Webcam
+```
+##### Para mudar o tamanho da tela do programa
+
+```python
+image = cv2.resize(image, None, fx=2, fy=2) #Tamanho da tela: dobro do default
+```
+##### Aperte `ESC` para sair
+
+```python
+cv2.waitKey(5) & 0xFF == 27: #Tecla ESC
+```
+
+# Limitações e Melhorias Futuras
+
+##### Este projeto foi criado como uma prova de conceito (PoC) e pode não funcionar perfeitamente em todos os ambientes ou para todos os usuários. Algumas possíveis melhorias e extensões futuras podem incluir:
+
+##### - Aumento da robustez dos gestos de controle, talvez usando uma variedade maior de gestos ou adicionando a capacidade de aprender novos gestos.
+##### - Testar e adaptar o programa para funcionar em uma variedade maior de iluminação e fundos.
+##### - Adicionar suporte para mais ações, como iniciar ou parar a apresentação de slides, além de simplesmente avançar ou retroceder.
+
+
+
+
+
+## Contribua 😃
+
+1 - Fork it
+
+2 - Cria sua feature branch (git checkout -b my-new-feature)
+
+3 - Commit suas mudanças (git commit -am "Added some feature")
+
+4 - Push na sua branch (git push origin my-new-feature)
+
+5 - Crie novo Pull Request
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pynput.keyboard import Key, Controller
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #Selecionar a WebCam
 kb = Controller()
 
 
@@ -21,7 +21,9 @@ start_time_direita = None
 start_time_esquerda = None
 
 
-with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(min_detection_confidence=0.5, 
+                    min_tracking_confidence=0.5) as hands:
+    
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -30,7 +32,7 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
 
         #Imagem horizontalmente para uma exibição posterior de visualização de selfie
         image = cv2.flip(image, 1)
-        image = cv2.resize(image, None, fx=2, fy=2) #Tamanhod da tela: dobro do default
+        image = cv2.resize(image, None, fx=2, fy=2) #Tamanho da tela: dobro do default
         image_height, image_width, _ = image.shape
    
         # Converte BGR imagem para RGB após processamento
@@ -63,13 +65,23 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                
                 if num_hands > 1:
                     print('Uma das mãos apenas')
-                    cv2.putText(image, 'Por favor, use apenas uma mao de cada vez.', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                    cv2.putText(image, 'Por favor, use apenas uma mao de cada vez.', 
+                               (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 
+                               2, cv2.LINE_AA)
 
 
                 # Se a mão for a direita e o polegar estiver acima dos outros dedos, é um gesto positivo
-                if handness == 'Right' and thumb_tip.y < index_finger_tip.y and thumb_tip.y < middle_finger_tip.y and thumb_tip.y < ring_finger_tip.y and thumb_tip.y < pinky_tip.y  and num_hands == 1:
+                if (handness == 'Right' and 
+                    thumb_tip.y < index_finger_tip.y and
+                    thumb_tip.y < middle_finger_tip.y and
+                    thumb_tip.y < ring_finger_tip.y and
+                    thumb_tip.y < pinky_tip.y  and
+                    num_hands == 1):
+
                     # Se este é o primeiro quadro no qual o gesto "positivo" é detectado, armazene o tempo atual e aciona a tecla direita
-                    if not positivo_detectado_direita and not positivo_detectado_esquerda:
+                    if (not positivo_detectado_direita and not
+                        positivo_detectado_esquerda):
+
                         start_time_direita = time.time()
                         print('Começou o tempo')
                         kb.press(Key.right)
@@ -80,7 +92,10 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                     print('.')
 
                      # Se o gesto "positivo" tem sido mantido por mais de 1 segundo, exibir a mensagem
-                    if positivo_detectado_direita and start_time_direita is not None and time.time() - start_time_direita >= 1:
+                    if (positivo_detectado_direita and 
+                        start_time_direita is not 
+                        None and time.time() - start_time_direita >= 1):
+
                         title = " - Abra a Palma"
                         print('Abra a Palma')
 
@@ -92,8 +107,16 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                
 
                
-                if handness == 'Left' and thumb_tip.y < index_finger_tip.y and thumb_tip.y < middle_finger_tip.y and thumb_tip.y < ring_finger_tip.y and thumb_tip.y < pinky_tip.y and num_hands == 1:
-                    if not positivo_detectado_esquerda and not positivo_detectado_direita : 
+                if (handness == 'Left' and 
+                    thumb_tip.y < index_finger_tip.y and 
+                    thumb_tip.y < middle_finger_tip.y and 
+                    thumb_tip.y < ring_finger_tip.y and 
+                    thumb_tip.y < pinky_tip.y and 
+                    num_hands == 1):
+
+                    if (not positivo_detectado_esquerda and not 
+                        positivo_detectado_direita): 
+
                         start_time_esquerda = time.time()
                         print('Começou o tempo')
                         kb.press(Key.left)
@@ -103,7 +126,10 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                     print('.')
 
                     
-                    if positivo_detectado_esquerda and start_time_esquerda is not None and time.time() - start_time_esquerda >= 1:
+                    if (positivo_detectado_esquerda and 
+                        start_time_esquerda is not None and 
+                        time.time() - start_time_esquerda >= 1):
+
                         title = "Abra a Palma"
                         print('Abre a palma') 
                    
@@ -119,19 +145,23 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                 y = int(thumb_tip.y * image_height)
 
                 # Desenhando um retângulo verde ao redor da mão
-                landmarks = [[int(landmark.x * image_width), int(landmark.y * image_height)] for landmark in hand_landmarks.landmark]
+                landmarks = [[int(landmark.x * image_width), 
+                            int(landmark.y * image_height)] for landmark in 
+                            hand_landmarks.landmark]
+                
                 landmarks = np.array(landmarks)
                 x_min, y_min = np.min(landmarks, axis=0)
                 x_max, y_max = np.max(landmarks, axis=0)
                 cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-                cv2.putText(image, title, (x_min, y_min-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                cv2.putText(image, title, (x_min, y_min-10), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
                 mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow('MediaPipe Hands', image)
 
-        if cv2.waitKey(5) & 0xFF == 27:
+        if cv2.waitKey(5) & 0xFF == 27:#Tecla ESC
             break
 
 cap.release()
